dc669e43e9ed971b8059ad205ca483eef641b0ef
Rickin Shah <rickin.shah17403@gmail.com>
2025-07-31 22:13:42 +0530

    Refactor config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,19 +3,20 @@ alias cat='bat'
 alias rm='rm -i'
 alias mkdir='mkdir -p'
 alias cd='z'
-zoxide init fish | source
-direnv hook fish | source
-if status is-interactive
 
-    function fish_command_not_found
-        if type -q yay
-            echo "Command '$argv' not found. Searching in packages..."
-            pacman -F $argv
-        else
-            echo "Command '$argv' not found and 'yay' is not installed."
-        end
+if status is-interactive
+    set -g fish_greeting
+    fastfetch
+    stty -icanon -echo
+    dd bs=1 count=1 >/dev/null 2>&1
+    stty icanon echo
+    zoxide init fish | source
+    direnv hook fish | source
+
+    function nvim
+        neovide $argv & disown
     end
-    # Commands to run in interactive sessions can go here
+
     function auto_activate_venv --on-variable PWD
         if test -f .venv/bin/activate.fish
             if not set -q VIRTUAL_ENV
@@ -25,10 +26,13 @@ if status is-interactive
             deactivate
         end
     end
+end
 
-    set -g fish_greeting
-    fastfetch
-    stty -icanon -echo
-    dd bs=1 count=1 >/dev/null 2>&1
-    stty icanon echo
+function fish_command_not_found
+    if type -q yay
+        echo "Command '$argv' not found. Searching in packages..."
+        pacman -F $argv
+    else
+        echo "Command '$argv' not found and 'yay' is not installed."
+    end
 end
